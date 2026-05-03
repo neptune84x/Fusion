dd3f459569ef95ff23df1fd39d7bb827c45aaf88
neptune84x <neptune84x0@gmail.com>
2026-05-03 14:05:01 +0300

    Refactor app initialization and run method

--- Sources/Fusion/main.swift
+++ Sources/Fusion/main.swift
@@ -1,6 +1,6 @@
 import AppKit
 
-let app = NSApplication.shared
+let app      = NSApplication.shared
 let delegate = AppDelegate()
 app.delegate = delegate
-_ = NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv)
+app.run()
